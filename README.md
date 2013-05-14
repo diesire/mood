5cd49899ca2e49b5e2159d4701307a24d2dc6d5e
Pablo Escalada Gómez <diesire@gmail.com>
2013-05-14 22:45:57 +0200

    Split mood library and server

---
+++ README.md
@@ -0,0 +1,27 @@
+# mood
+
+Twitter feelings analizer
+
+## Getting Started
+Install the module with: `npm install mood`
+
+```javascript
+var mood = require('mood');
+mood.rate('Bueno'); //6
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Pablo Escalada Gómez  
+Licensed under the MIT license.
---
+++ test/mood_test.js
@@ -0,0 +1,86 @@
+/*
+ * mood_test
+ * https://github.com/diesire/mood
+ *
+ * Copyright (c) 2013 Pablo Escalada Gómez
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var mood = require('../lib/mood.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports['test'] = {
+    setUp: function (done) {
+        done();
+    },
+
+    'Rate undefined text': function (test) {
+        test.expect(1);
+        test.throws(function () {
+            mood.rate();
+        }, Error, 'should throw an Error');
+        test.done();
+    },
+
+    'Rate empty text': function (test) {
+        test.expect(1);
+        test.throws(function () {
+            mood.rate("");
+        }, Error, 'should throw an Error');
+        test.done();
+    },
+
+    'Rate simple text': function (test) {
+        var hi = 'hi';
+
+        test.expect(1);
+        test.equal(mood.rate(hi), hi.length, 'should be text.lenght');
+        test.done();
+    },
+
+    'Tokenize simple text': function (test) {
+        var helloWorld = 'Hola, mundo!';
+
+        test.expect(1);
+        test.deepEqual(mood.tools.tokenizer(helloWorld), ['Hola', 'mundo'], 'should be [Hola, mundo]');
+        test.done();
+    },
+    
+    'Stemmize simple text': function (test) {
+        var helloWorld = 'Adiós mundo cruel!';
+
+        test.expect(1);
+        test.deepEqual(mood.tools.stemmer(helloWorld), ['adi', 'mundo', 'cruel'], 'should be [adi, mundo, cruel]');
+        test.done();
+    },
+    
+    'Detect spanish text': function (test) {
+        var text = 'En un lugar de la Mancha de cuyo nombre no quiero acordarme';
+        var language = mood.tools.language(text);
+
+        test.expect(1);
+        test.deepEqual(language[0][0], "spanish" , 'should be spanish');
+        test.done();
+    }    
+};
