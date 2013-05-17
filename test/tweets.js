fbec3571ad8e2e5977d776fdb502498c8ad579f9
Pablo Escalada Gómez <diesire@gmail.com>
2013-05-17 20:08:14 +0200

    Add some collection of tweets

---
+++ test/tweets.js
@@ -0,0 +1,150 @@
+//http://156.35.98.15/consulta_tweets.php?palabra=universo&positivo=2.0&negativo=3.0&fecha=2008-02-02
+exports.set1 = [{
+        "id": "2030890",
+        "fecha": "2009-08-20T00:3",
+        "nombre": "mishijordan (Michael jordan)",
+        "tweet": "¿que somos? ¿quienes somos? ¿que es el mundo? ¿que es el universo? ¿parte de que es el universo? ¿que hay en las fronteras del universo?",
+        "status": "http:\/\/twitter.com\/mishijordan\/statuses\/3416825741\r"
+    }, {
+        "id": "1939577",
+        "fecha": "2009-08-19T05:0",
+        "nombre": "maximovoz (maximovoz)",
+        "tweet": "Cómo Crear tu Propio Universo AHORA: Para cada uno de nosotros, el universo es diferente.\r",
+        "status": ""
+    }, {
+        "id": "774976",
+        "fecha": "2009-08-08T04:2",
+        "nombre": "ultravpsycho (nayeli rivera)",
+        "tweet": "como el pretzel más salado del universo después de la botella de cavernet más cara del universo",
+        "status": "http:\/\/twitter.com\/ultravpsycho\/statuses\/3188934830\r"
+    }, {
+        "id": "2433071",
+        "fecha": "2009-08-27T02:5",
+        "nombre": "addkerberos (लुवुस)",
+        "tweet": "\"El universo está lleno de cosas raras e interesantes\" El Universo, History Channel. Excelente :)",
+        "status": "http:\/\/twitter.com\/addkerberos\/statuses\/3571314405\r"
+    }, {
+        "id": "857837",
+        "fecha": "2009-08-09T02:2",
+        "nombre": "dr_cutestein (Nicolás Azuara)",
+        "tweet": "Quién ganaría entre el universo de Battlestar Galactica contra el universo Star Wars?",
+        "status": "http:\/\/twitter.com\/dr_cutestein\/statuses\/3201238809\r"
+    }, {
+        "id": "63373",
+        "fecha": "2009-08-01T14:5",
+        "nombre": "poleth (poleth)",
+        "tweet": "...y el universo, creando cada vez más y mejores idiotas. De momento gana el UNIVERSO.",
+        "status": "http:\/\/twitter.com\/poleth\/statuses\/3070747039\r"
+    }, {
+        "id": "827234",
+        "fecha": "2009-08-08T20:1",
+        "nombre": "fredericopaiva (Frederico Paiva)",
+        "tweet": "de curvas é feito todo o universo...o universo curvo de einstein . niemeyer",
+        "status": "http:\/\/twitter.com\/fredericopaiva\/statuses\/3197228107\r"
+    }, {
+        "id": "1248900",
+        "fecha": "2009-08-12T20:1",
+        "nombre": "Slayer_X (Cesar Villegas)",
+        "tweet": "Q tipo de ing inteligente crearía un universo q necesita 13k millones de años para generar al hombre, desperdiciando el 99,99% del universo?",
+        "status": "http:\/\/twitter.com\/Slayer_X\/statuses\/3272109118\r"
+    }, {
+        "id": "2429873",
+        "fecha": "2009-08-27T02:2",
+        "nombre": "kenya324com (Alexandra Dejesus)",
+        "tweet": "¡¡FELIZ CUMPLEAÑOS THALIA QUE EL UNIVERSO TE LLENE DE DICHA Y SALUD Y TE PROSPERE EN LO MAS BELLO QUE TIENE EL UNIVERSO PARA TI!!",
+        "status": "http:\/\/twitter.com\/kenya324com\/statuses\/3570681477\r"
+    }, {
+        "id": "1814027",
+        "fecha": "2009-08-18T03:0",
+        "nombre": "eileenornot (Eileen Morán)",
+        "tweet": "Por primera vez estoy consciente de ser la autora de mi propio universo. Y ese universo, good heavens, me gusta! (cheering firework display)",
+        "status": "http:\/\/twitter.com\/eileenornot\/statuses\/3375303309\r"
+    }, {
+        "id": "857689",
+        "fecha": "2009-08-09T02:2",
+        "nombre": "cleek (Miguel Gabriel^.\/.^)",
+        "tweet": "Hoy probé el strudel más delicioso de todo el universo ¡el universo!",
+        "status": "http:\/\/twitter.com\/cleek\/statuses\/3201215742\r"
+    }, {
+        "id": "1219560",
+        "fecha": "2009-08-12T14:4",
+        "nombre": "ramonstyle (ramon moysés)",
+        "tweet": "@marcioballas essa faculdades existe só q UNIVERSO é sigla Universidade Salgado de Oliveira.Universo tbm é nome d time de baskt de Brasilia",
+        "status": "http:\/\/twitter.com\/ramonstyle\/statuses\/3266167528\r"
+    }, {
+        "id": "1525815",
+        "fecha": "2009-08-15T05:5",
+        "nombre": "rockstalgica (Mike Aguilar)",
+        "tweet": "Al mirar al universo de tus ojos ,veo el universo de tus sentimientos",
+        "status": "http:\/\/twitter.com\/rockstalgica\/statuses\/3323938678\r"
+    }, {
+        "id": "1323416",
+        "fecha": "2009-08-13T13:3",
+        "nombre": "k40s (Alberto)",
+        "tweet": "Nuevo universo en Ogame, se llama universo andromeda y de momento la unica mejora que he visto es la interfaz. mmm Ogame, que recuerdos",
+        "status": "http:\/\/twitter.com\/k40s\/statuses\/3285940076\r"
+    }, {
+        "id": "1533954",
+        "fecha": "2009-08-15T09:3",
+        "nombre": "planetamafra (planetamafra)",
+        "tweet": "O Universo de Morton: A.E. Van Vogt (1994). O Universo de Morton. Lisboa: Livros do Brasil.Fantastic Fiction &#1.. http:\/\/bit.ly\/gCQ6C",
+        "status": "http:\/\/twitter.com\/planetamafra\/statuses\/3325979095\r"
+    }, {
+        "id": "1883555",
+        "fecha": "2009-08-18T19:1",
+        "nombre": "ensiz_news (Enrique Sierra)",
+        "tweet": "Latinas se ubican como favoritas en Miss Universo: Según los organizadores de Miss Universo, nueve bellezas lati.. http:\/\/bit.ly\/3lSjle",
+        "status": "http:\/\/twitter.com\/ensiz_news\/statuses\/3388442401\r"
+    }, {
+        "id": "444880",
+        "fecha": "2009-08-05T01:5",
+        "nombre": "paulotenino (paulo rodrigo)",
+        "tweet": "RT @eduardoe: RT @twiterillo: 99 datos curiosos de la ciencia y el universo http:\/\/rt.nu\/h2ep #science #ciencia #universe #universo",
+        "status": "http:\/\/twitter.com\/paulotenino\/statuses\/3136585143\r"
+    }, {
+        "id": "1864685",
+        "fecha": "2009-08-18T15:5",
+        "nombre": "barrosromo (José Barros)",
+        "tweet": "MISS UNIVERSO: Latinas son las favoritas a ceñirse la corona de Miss Universo, ya eligieron a las 15 semifinalistas...http:\/\/bit.ly\/LAGyP",
+        "status": "http:\/\/twitter.com\/barrosromo\/statuses\/3384703745\r"
+    }, {
+        "id": "1188579",
+        "fecha": "2009-08-12T05:3",
+        "nombre": "periodistas (Periodistas)",
+        "tweet": "Una cadena de Miami despide a periodista gay tras una demanda por ... - Universo Gay: Universo GayUna cadena de .. http:\/\/bit.ly\/IGFpR",
+        "status": "http:\/\/twitter.com\/periodistas\/statuses\/3260440756\r"
+    }, {
+        "id": "1352802",
+        "fecha": "2009-08-13T19:0",
+        "nombre": "wwwcombo (Joel Duran)",
+        "tweet": "MISS UNIVERSO: Dayana Mendoza, actual Miss Universo, hará topless en el número de septiembre de la revista Maxim.",
+        "status": "http:\/\/twitter.com\/wwwcombo\/statuses\/3291720129\r"
+    }
+];
+
+exports.negativeSet = [{
+        "id": "1352802",
+        "fecha": "2009-08-13T19:0",
+        "nombre": "wwwcombo (Joel Duran)",
+        "tweet": "malo negro llorón feo",
+        "status": "http:\/\/twitter.com\/wwwcombo\/statuses\/3291720129\r"
+    }
+];
+
+exports.neutralSet = [{
+        "id": "1352802",
+        "fecha": "2009-08-13T19:0",
+        "nombre": "wwwcombo (Joel Duran)",
+        "tweet": "lote",
+        "status": "http:\/\/twitter.com\/wwwcombo\/statuses\/3291720129\r"
+    }
+];
+
+exports.positiveSet = [{
+        "id": "1352802",
+        "fecha": "2009-08-13T19:0",
+        "nombre": "wwwcombo (Joel Duran)",
+        "tweet": " cielo universo bueno bonito",
+        "status": "http:\/\/twitter.com\/wwwcombo\/statuses\/3291720129\r"
+    }
+];
